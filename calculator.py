ded898b611b17c99e3a63cea7b82631f97cfe137
Stewart Rice <141388688+cyberrhyc@users.noreply.github.com>
2026-04-03 10:19:11 +0200

    Added subtraction

--- calculator.py
+++ calculator.py
@@ -1,4 +1,20 @@
-a:int = int(input("Enter a: "))
-b:int = int(input("Enter b: "))
-c:int = a + b   
-print("The sum of a and b is: ", c)
+
+
+def add() -> int:
+    a:int = int(input("Enter a: "))
+    b:int = int(input("Enter b: "))
+    c:int = a + b   
+    return c
+
+def subtract() -> int:
+    a:int = int(input("Enter a: "))
+    b:int = int(input("Enter b: "))
+    c:int = a - b
+    return c
+
+def main():
+    a=input("Enter mode \n 1. Add\n 2. Subtract\n")
+    if (a=="1"):
+        print(add())
+    elif (a=="2"):
+        print(subtract())
