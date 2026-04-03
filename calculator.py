c106f3f01b2e3388131d2cd8c277433f06904858
Stewart Rice <141388688+cyberrhyc@users.noreply.github.com>
2026-04-03 10:26:48 +0200

    y

--- calculator.py
+++ calculator.py
@@ -6,13 +6,11 @@ def add() -> int:
     c:int = a + b   
     return c
 
-
 def subtract() -> int:
     a:int = int(input("Enter a: "))
     b:int = int(input("Enter b: "))
     c:int = a - b
     return c
-
 
 def main():
     a=input("Enter mode \n 1. Add\n 2. Subtract\n")
@@ -20,6 +18,4 @@ def main():
         print(add())
     elif (a=="2"):
         print(subtract())
-
-
-main()
+        
